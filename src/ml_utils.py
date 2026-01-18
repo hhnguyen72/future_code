f4f91a747b2eb9fddc7156333ea3f74b74d6e8be
Hung Nguyen <hung.h.nguyen72@gmail.com>
2026-01-18 11:14:11 -0500

    Clean State -- NO pyproject

--- src/ml_utils.py
+++ src/ml_utils.py
@@ -1,3 +1,36 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import (
+    train_test_split,
+    GridSearchCV,
+    cross_val_score,
+    StratifiedKFold
+)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cluster import KMeans
+from sklearn.ensemble import (
+    RandomForestRegressor,
+    RandomForestClassifier,
+    GradientBoostingClassifier
+)
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    roc_curve,
+    roc_auc_score,
+    auc,
+    precision_recall_curve,
+    ConfusionMatrixDisplay
+)
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import (
+    LabelEncoder,
+    StandardScaler,
+    MinMaxScaler,
+    OneHotEncoder
+)
+from sklearn.pipeline import Pipeline
